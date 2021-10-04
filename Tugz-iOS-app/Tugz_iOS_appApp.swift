b62c1da9855fde72b4dd5f4d49ab142b14555220
Charlie Williams <c@harl.ie>
2021-10-04 16:51:57 +0100

    Initial Commit

---
+++ Tugz-iOS-app/Tugz_iOS_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tugz_iOS_appApp.swift
+//  Tugz-iOS-app
+//
+//  Created by Charlie Williams on 04/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct Tugz_iOS_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
