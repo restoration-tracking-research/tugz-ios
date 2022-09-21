c45f107c4c826f84111e2d600f01134ce588cbe0
Charlie Williams <c@harl.ie>
2022-09-21 09:04:59 +0100

    WIP

---
+++ Tugz-Intents-Extension/IntentHandler.swift
@@ -0,0 +1,19 @@
+//
+//  IntentHandler.swift
+//  Tugz-Intents-Extension
+//
+//  Created by Charlie Williams on 13/09/2022.
+//
+
+import Intents
+
+class IntentHandler: INExtension {
+    
+    override func handler(for intent: INIntent) -> Any {
+        // This is the default implementation.  If you want different objects to handle different intents,
+        // you can override this and return the handler you want for that particular intent.
+        
+        return self
+    }
+    
+}
